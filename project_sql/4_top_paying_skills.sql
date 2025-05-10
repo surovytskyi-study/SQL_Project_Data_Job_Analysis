f205b5c101f93adb6c7c02c495c8459be8a3c21e
surovytskyi-study <suriken.00707@gmail.com>
2025-05-10 15:15:18 +0300

    added readme and images

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -1,13 +1,14 @@
 
 SELECT 
 	skills,
-	AVG(jpf.salary_year_avg) as salary_avg
+	ROUND(AVG(jpf.salary_year_avg), 0) as salary_avg
 FROM job_postings_fact as jpf
 INNER JOIN skills_job_dim as sjd ON jpf.job_id = sjd.job_id
 INNER JOIN skills_dim skills ON sjd.skill_id = skills.skill_id
 WHERE
 	job_title_short = 'Data Analyst' AND
-	jpf.salary_year_avg IS NOT NULL
+	jpf.salary_year_avg IS NOT NULL AND
+	jpf.job_work_from_home = TRUE
 GROUP BY
 	skills
 ORDER BY
